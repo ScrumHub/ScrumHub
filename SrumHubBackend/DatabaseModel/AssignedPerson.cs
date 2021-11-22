744a5f3d85e689ed86b19c9d24f28f494602cc08
Andrzej Litkowski <lank891@gmail.com>
2021-11-22 22:45:10 +0100

    Databse structure without preceeding tasks

---
+++ SrumHubBackend/DatabaseModel/AssignedPerson.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScrumHubBackend.DatabaseModel
+{
+    /// <summary>
+    /// Represents person assigned to the task
+    /// </summary>
+    [Table("assigned_person")]
+    public class AssignedPerson
+    {
+        /// <summary>
+        /// Internal id of an assignment
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Id of a person
+        /// </summary>
+        [Required]
+        public long PersonGitHubId { get; set; } = 0;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AssignedPerson() { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AssignedPerson(long personGitHubId)
+        {
+            PersonGitHubId = personGitHubId;
+        }
+    }
+}
