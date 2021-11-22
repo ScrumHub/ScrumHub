4ecddaa6f55927b1896a48c5de6fe4293c1fa88b
Andrzej Litkowski <lank891@gmail.com>
2021-11-22 22:45:10 +0100

    Databse structure without preceeding tasks

---
+++ SrumHubBackend/DatabaseModel/AcceptanceCriterium.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScrumHubBackend.DatabaseModel
+{
+    /// <summary>
+    /// Represents one acceptance criterium
+    /// </summary>
+    [Table("acceptance_criterium")]
+    public class AcceptanceCriterium
+    {
+
+        /// <summary>
+        /// Internal id of criterium
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Criterium text
+        /// </summary>
+        [Required]
+        public string Text { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AcceptanceCriterium() { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AcceptanceCriterium(string criterium)
+        {
+            Text = criterium;
+        }
+    }
+}
