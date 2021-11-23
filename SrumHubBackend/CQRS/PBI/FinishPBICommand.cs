9b0149ad70270e9e0c65dd70a1b6c542b6e26340
Andrzej Litkowski <lank891@gmail.com>
2021-11-24 00:52:23 +0100

    Finishing the PBI

---
+++ SrumHubBackend/CQRS/PBI/FinishPBICommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using ScrumHubBackend.CommunicationModel;
+
+namespace ScrumHubBackend.CQRS.PBI
+{
+    /// <summary>
+    /// Command for finishing one PBI
+    /// </summary>
+    public class FinishPBICommand : IRequest<BacklogItem>
+    {
+        /// <summary>
+        /// Github authorization token
+        /// </summary>
+        public string? AuthToken { get; set; }
+
+        /// <summary>
+        /// Owner of the repository
+        /// </summary>
+        public string? RepositoryOwner { get; set; }
+
+        /// <summary>
+        /// Name of the repository
+        /// </summary>
+        public string? RepositoryName { get; set; }
+
+        /// <summary>
+        /// Id of the PBI
+        /// </summary>
+        public long PBIId { get; set; }
+    }
+}
