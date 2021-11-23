b0777d3d4d9cf91e55f466d8ebc86e5c199e73b3
Andrzej Litkowski <lank891@gmail.com>
2021-11-24 00:21:06 +0100

    Getting one PBI

---
+++ SrumHubBackend/CQRS/PBI/GetOnePBIQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using ScrumHubBackend.CommunicationModel;
+using ScrumHubBackend.CustomExceptions;
+using ScrumHubBackend.GitHubClient;
+
+namespace ScrumHubBackend.CQRS.PBI
+{
+    /// <summary>
+    /// Handler for getting one PBI
+    /// </summary>
+    public class GetOnePBIQueryHandler : IRequestHandler<GetOnePBIQuery, BacklogItem>
+    {
+        private readonly ILogger<GetOnePBIQueryHandler> _logger;
+        private readonly IGitHubClientFactory _gitHubClientFactory;
+        private readonly DatabaseContext _dbContext;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public GetOnePBIQueryHandler(ILogger<GetOnePBIQueryHandler> logger, IGitHubClientFactory clientFactory, DatabaseContext dbContext)
+        {
+            _logger = logger ?? throw new ArgumentException(null, nameof(logger));
+            _dbContext = dbContext ?? throw new ArgumentException(null, nameof(dbContext));
+            _gitHubClientFactory = clientFactory ?? throw new ArgumentException(null, nameof(clientFactory));
+        }
+
+        /// <inheritdoc/>
+        public Task<BacklogItem> Handle(GetOnePBIQuery request, CancellationToken cancellationToken)
+        {
+            if (request == null || request.AuthToken == null)
+                throw new BadHttpRequestException("Missing token");
+
+            var gitHubClient = _gitHubClientFactory.Create(request.AuthToken);
+
+            // If it does not exists then user does not have permissions to read id
+            var repository = gitHubClient.Repository.Get(request.RepositoryOwner, request.RepositoryName).Result;
+
+            var dbRepository = _dbContext.Repositories?.FirstOrDefault(repo => repo.FullName == repository.FullName);
+
+            if (dbRepository == null)
+                throw new NotFoundException("Repository not found in ScrumHub");
+
+            var pbi = dbRepository?.BacklogItems?.FirstOrDefault(pbi => pbi.Id == request.PBIId) ?? null;
+
+            if(pbi == null)
+                throw new NotFoundException("Backlog item not found in ScrumHub");
+
+            return Task.FromResult(new BacklogItem(pbi.Id, _dbContext));
+        }
+    }
+}
