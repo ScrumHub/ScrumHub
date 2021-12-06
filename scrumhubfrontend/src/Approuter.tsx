378b6016122b5dc3e5526e296428461fa41c3f6a
alekk12 <a7kolakowska@gmail.com>
2021-12-06 22:56:23 +0100

    added sprint backlog

--- scrumhubfrontend/src/Approuter.tsx
+++ scrumhubfrontend/src/Approuter.tsx
@@ -4,6 +4,7 @@ import Teams from "./components/Teams";
 import Project from "./components/Project";
 import Home from "./components/Home";
 import SprintList from "./components/SprintList";
+import SprintBacklog from "./components/SprintBacklog";
 const AppRouter = () => {
     return (
         <Routes>
@@ -12,7 +13,7 @@ const AppRouter = () => {
             <Route path="/teams" element={<Teams/>}/>
             <Route path={`/:owner/:name`} element={<Project/>}/>
             <Route path={`/:owner/:name/sprints`} element={<SprintList/>}/>
-            {/*<Route path={`/:owner/:name/sprints/:number`} element={<SprintBacklog/>}/>*/}
+            {<Route path={`/:owner/:name/sprints/:number`} element={<SprintBacklog/>}/>}
         </Routes>
     );
 }
