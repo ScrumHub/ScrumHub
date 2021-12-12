9eedda153762da45db0cbc6dc2b9747d2d597c9a
Andrzej Litkowski <lank891@gmail.com>
2021-12-12 20:35:08 +0100

    Person return model

---
+++ ScrumHubBackend/CommunicationModel/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace ScrumHubBackend.CommunicationModel
+{
+    /// <summary>
+    /// Represents person
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Name of person (might be null)
+        /// </summary>
+        public string? Name { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Login of person
+        /// </summary>
+        public string? Login { get; set; } = String.Empty;
+
+        /// <summary>
+        /// GitHub id of person
+        /// </summary>
+        public long GitHubId { get; set; } = 0;
+
+        /// <summary>
+        /// Link to the avatar of the person
+        /// </summary>
+        public string? AvatarLink { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Person(Octokit.User user)
+        {
+            Name = user.Name;
+            Login = user.Login;
+            GitHubId = user.Id;
+            AvatarLink = user.AvatarUrl;
+        }
+    }
+}
