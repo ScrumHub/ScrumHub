debe619f344f8024dcee7298613e2cd91f3bcc6b
Andrzej Litkowski <lank891@gmail.com>
2021-12-12 20:35:36 +0100

    Controller for people

---
+++ ScrumHubBackend/Controllers/PeopleController.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ScrumHubBackend.CommunicationModel;
+using ScrumHubBackend.CommunicationModel.Common;
+using ScrumHubBackend.CQRS.People;
+using System.Net;
+
+namespace ScrumHubBackend.Controllers
+{
+    /// <summary>
+    /// Controller for people
+    /// </summary>
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly ILogger<PeopleController> _logger;
+        private readonly IMediator _mediator;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public PeopleController(ILogger<PeopleController> logger, IMediator mediator)
+        {
+            _logger = logger ?? throw new ArgumentException(null, nameof(logger));
+            _mediator = mediator ?? throw new ArgumentException(null, nameof(mediator));
+        }
+
+        /// <summary>
+        /// Gets users for given repository (requires read permissions in repository)
+        /// </summary>
+        /// <param name="authToken">Authorization token of user</param>
+        /// <param name="repositoryOwner">Owner of the repository</param>
+        /// <param name="repositoryName">Name of the repository</param>
+        [HttpGet("{repositoryOwner}/{repositoryName}")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(PaginatedList<Person>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetBacklogItems(
+            [FromHeader] string authToken,
+            [FromRoute] string repositoryOwner,
+            [FromRoute] string repositoryName
+            )
+        {
+            var query = new GetPeopleQuery
+            {
+                AuthToken = authToken,
+                RepositoryOwner = repositoryOwner,
+                RepositoryName = repositoryName,
+            };
+
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
