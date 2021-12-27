79942ef84d38a070adfe70778e34f6eb4684eb4e
Andrzej Litkowski <lank891@gmail.com>
2021-12-27 09:54:49 +0100

    Command and handler for finishing Sprint

---
+++ ScrumHubBackend/CQRS/Sprints/FinishSprintCommand.cs
@@ -0,0 +1,20 @@
+﻿using ScrumHubBackend.CommunicationModel;
+
+namespace ScrumHubBackend.CQRS.Sprints
+{
+    /// <summary>
+    /// Command for finishing sprint
+    /// </summary>
+    public class FinishSprintCommand : CommonInRepositoryRequest<Sprint>
+    {
+        /// <summary>
+        /// Number of the sprint
+        /// </summary>
+        public long SprintNumber { get; set; }
+
+        /// <summary>
+        /// True if sprint was failed, false if it was success
+        /// </summary>
+        public bool IsFailure { get; set; }
+    }
+}
---
+++ ScrumHubBackend/CQRS/Sprints/FinishSprintCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using ScrumHubBackend.CommunicationModel;
+using ScrumHubBackend.CustomExceptions;
+using ScrumHubBackend.GitHubClient;
+
+namespace ScrumHubBackend.CQRS.Sprints
+{
+    /// <summary>
+    /// Handler for finishing sprint
+    /// </summary>
+    public class FinishSprintCommandHandler : IRequestHandler<FinishSprintCommand, Sprint>
+    {
+        private readonly ILogger<FinishSprintCommandHandler> _logger;
+        private readonly IGitHubClientFactory _gitHubClientFactory;
+        private readonly DatabaseContext _dbContext;
+        private readonly IMediator _mediator;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FinishSprintCommandHandler(ILogger<FinishSprintCommandHandler> logger, IGitHubClientFactory clientFactory, DatabaseContext dbContext, IMediator mediator)
+        {
+            _logger = logger ?? throw new ArgumentException(null, nameof(logger));
+            _dbContext = dbContext ?? throw new ArgumentException(null, nameof(dbContext));
+            _gitHubClientFactory = clientFactory ?? throw new ArgumentException(null, nameof(clientFactory));
+            _mediator = mediator ?? throw new ArgumentException(null, nameof(mediator));
+        }
+
+        /// <inheritdoc/>
+        public Task<Sprint> Handle(FinishSprintCommand request, CancellationToken cancellationToken)
+        {
+            if (request == null || request.AuthToken == null)
+                throw new BadHttpRequestException("Missing token");
+
+            var gitHubClient = _gitHubClientFactory.Create(request.AuthToken);
+
+            var repository = gitHubClient.Repository.Get(request.RepositoryOwner, request.RepositoryName).Result;
+
+            var dbRepository = _dbContext.Repositories?.FirstOrDefault(repo => repo.FullName == repository.FullName);
+
+            if (dbRepository == null)
+                throw new NotFoundException("Repository not found in ScrumHub");
+
+            if (!repository.Permissions.Admin)
+                throw new ForbiddenException("Not enough permissions to finish a PBI to repository");
+
+            var sprintsForRepository = dbRepository.GetSprintsForRepository(_dbContext);
+            var dbSprint = sprintsForRepository?.FirstOrDefault(sprint => sprint.SprintNumber == request.SprintNumber) ?? null;
+
+            if (dbSprint == null)
+                throw new NotFoundException("Sprint not fount in the repository");
+
+            dbSprint.Status = request.IsFailure ? Common.SprintStatus.Failed : Common.SprintStatus.Successful;
+
+            _dbContext.Update(dbSprint);
+            _dbContext.SaveChanges();
+
+            return Task.FromResult(new Sprint(dbSprint, request, _dbContext, _mediator));
+        }
+    }
+}
