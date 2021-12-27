300f3f43cd446f5f2355fe1b14e3825a015dd4ec
Andrzej Litkowski <lank891@gmail.com>
2021-12-27 09:48:10 +0100

    Model changes (status of sprint)

---
+++ ScrumHubBackend/Common/SprintStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace ScrumHubBackend.Common
+{
+    /// <summary>
+    /// Status of the sprint
+    /// </summary>
+    public enum SprintStatus
+    {
+        /// <summary>
+        /// Sprint that is not finished yet
+        /// </summary>
+        NotFinished,
+
+        /// <summary>
+        /// Sprint marked as failed
+        /// </summary>
+        Failed,
+
+        /// <summary>
+        /// Sprint marked as success
+        /// </summary>
+        Successful
+    }
+}
